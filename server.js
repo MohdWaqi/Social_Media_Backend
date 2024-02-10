42d6d44dd483e72f6ea1cd0d05ab88f239006ca8
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2024-02-10 22:26:24 +0530

    deploy customizations

--- server.js
+++ server.js
@@ -5,17 +5,24 @@ const blogRoute = require('./routes/blogRoutes')
 const authRoute = require('./routes/authorization')
 
 
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.mongoURL);
+      console.log('MongoDB Connected');
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
 
 const app = express();
 app.use(express.json());
 app.use('/users', authRoute)
 app.use('/posts', blogRoute);
 
-app.listen(process.env.PORT, ()=>{
-    try {
-        mongoose.connect(process.env.mongoURL)
-        console.log(`Server listening on port ${process.env.PORT} and mongo is connected!`);
-    } catch (error) {
-        console.log(error)
-    }
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("listening for requests");
+    })
 })
